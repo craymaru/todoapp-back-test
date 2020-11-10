62fa802e633a603570765702cd0cd806fbf9a067
Cray <craymaru@gmail.com>
2020-11-10 14:09:01 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from chalice import Chalice
+from chalice import BadRequestError, NotFoundError
+from chalicelib import database
+
+app = Chalice(app_name='todo_back')
+
+
+@app.route('/todos', methods=['GET'], cors=True)
+def get_all_todos():
+    return database.get_all_todos()
+
+
+@app.route('/todos/{todo_id}', methods=['GET'], cors=True)
+def get_todo(todo_id):
+    todo = database.get_todo(todo_id)
+    if todo:
+        return todo
+    else:
+        raise NotFoundError('Todo not found.')  # 404
+
+
+@app.route('/todos', methods=['POST'], cors=True)
+def create_todo():
+    # リクエストメッセージjson_bodyを修得
+    todo = app.current_request.json_body
+
+    # 必須項目をチェック
+    for key in ['title', 'memo', 'priority']:
+        if key not in todo:
+            raise BadRequestError(f"{key} is required.")
+
+    # データを登録
+    return database.create_todo(todo)
+
+
+@app.route('/todos/{todo_id}', methods=['PUT'], cors=True)
+def update_todo(todo_id):
+    changes = app.current_request.json_body
+
+    # データを更新
+    return database.update_todo(todo_id, changes)
+
+
+@app.route('/todos/{todo_id}', methods=['DELETE'], cors=True)
+def delete_todo(todo_id):
+    # データを削除
+    return database.delete_todo(todo_id)
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ chalicelib/database.py
@@ -0,0 +1,83 @@
+import os
+from os import terminal_size
+import boto3
+from boto3.dynamodb.conditions import Key
+import uuid
+
+
+def _get_database():
+    # DynamoDBへの接続を修得する
+    endpoint = os.environ.get('DB_ENDPOINT')
+    if endpoint:
+        return boto3.resource('dynamodb', endpoint_url=endpoint)
+    else:
+        return boto3.resource('dynamodb')
+
+
+def get_all_todos():
+    # すべてのレコードを修得
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    response = table.scan()
+    return response['Items']
+
+
+def get_todo(todo_id):
+    # IDからレコードを修得
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(todo_id)
+    )
+    items = response['Items']
+    return items[0] if items else None
+
+
+def create_todo(todo):
+    # 登録内容を作成
+    item = {
+        'id': uuid.uuid4().hex,
+        'title': todo['title'],
+        'memo': todo['memo'],
+        'priority': todo['priority'],
+        'completed': False,
+    }
+
+    # DynamoDB にデータ登録
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    table.put_item(Item=item)
+    return item
+
+
+def update_todo(todo_id, changes):
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+
+    # クエリを構築
+    update_expression = []
+    expression_attribute_values = {}
+    for key in ['title', 'memo', 'priority', 'completed']:
+        if key in changes:
+            update_expression.append(f"{key} = :{key[0:1]}")
+            expression_attribute_values[f":{key[0:1]}"] = changes[key]
+
+    # DynamoDB のデータを更新
+    result = table.update_item(
+        Key={
+            'id': todo_id,
+        },
+        UpdateExpression='set ' + ','.join(update_expression),
+        ExpressionAttributeValues=expression_attribute_values,
+        ReturnValues='ALL_NEW'
+    )
+    return result['Attributes']
+
+
+def delete_todo(todo_id):
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+
+    # DynamoDB のデータを削除
+    result = table.delete_item(
+        Key={
+            'id': todo_id,
+        },
+        ReturnValues='ALL_OLD'
+    )
+    return result['Attributes']
